b11f3cfdf2fd7cc96aa6e024be050dbabe2193bc
Joaquin <jaltable@fi.uba.ar>
2023-09-28 14:34:40 -0300

    added content to 'NavBar'

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -3,7 +3,15 @@ import styles from './NavBar.module.css';
 export default function NavBar(){
     return(
         <>
-        Nav
+        <a href="#">Logo</a>
+
+        <button>Home</button>
+        <button>Market</button>
+        <button>Choose Us</button>
+        <button>Join</button>
+
+        <a href="#">Twitter</a>
+        <a href="#">Discord</a>
         </>
     );
 }
