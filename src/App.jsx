ae0cb476e80896b645377245abbf170a613adcfd
Joaquin <jaltable@fi.uba.ar>
2023-09-28 14:33:07 -0300

    created 'NavBar' component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css';
 
 import LandingSection from './Components/LandingSection/LandingSection';
+import NavBar from './Components/NavBar/NavBar';
 
 function App() {
 
   return (
     <>
+    <NavBar/>
+
     <LandingSection/>
     </>
   )
