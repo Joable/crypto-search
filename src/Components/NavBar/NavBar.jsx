13adf91fd74420ac0e428c3ab65a9be4638b7e2e
Joaquin <jaltable@fi.uba.ar>
2023-09-29 17:13:06 -0300

    centered 'NavBar'

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -2,16 +2,30 @@ import styles from './NavBar.module.css';
 
 export default function NavBar(){
     return(
-        <>
-        <a href="#">Logo</a>
+        <div className={styles.navBar}>
 
-        <button>Home</button>
-        <button>Market</button>
-        <button>Choose Us</button>
-        <button>Join</button>
+            <div className={styles.navWrapper}>
 
-        <a href="#">Twitter</a>
-        <a href="#">Discord</a>
-        </>
+                <a href="#">Logo</a>
+
+                <div className={styles.navButtons}>
+
+                    <button>Home</button>
+                    <button>Market</button>
+                    <button>Choose Us</button>
+                    <button>Join</button>
+
+                </div>
+
+                <div className={styles.navSocial}>
+
+                    <a href="#">Twitter</a>
+                    <a href="#">Discord</a>
+                
+                </div>
+
+            </div>
+        
+        </div>
     );
 }
