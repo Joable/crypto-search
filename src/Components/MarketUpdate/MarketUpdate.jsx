25925a0bf5d64b7bf0c4693ca8ce0b60e1c82a83
Joaquin <jaltable@fi.uba.ar>
2023-10-02 19:45:05 -0300

    added divitions to 'MarketUpdate'

--- src/Components/MarketUpdate/MarketUpdate.jsx
+++ src/Components/MarketUpdate/MarketUpdate.jsx
@@ -3,7 +3,19 @@ import styles from './MarketUpdate.module.css';
 export default function MarketUpdate(){
     return(
         <>
-        Market Update
+        <h2>Market Update</h2>
+
+        <div className={styles.divitions}>
+
+            <h3>Coin</h3>
+
+            <h3>Price</h3>
+
+            <h3>24h change</h3>
+
+            <h3>Market Cap</h3>
+
+        </div>
         </>
     );
 }
