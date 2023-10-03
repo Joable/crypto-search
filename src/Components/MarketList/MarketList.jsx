0a408c0d86601ebdc54725a74615f775d29e74c7
Joaquin <jaltable@fi.uba.ar>
2023-10-03 17:20:39 -0300

    'MarketList' displays the first five elements of 'array'

--- src/Components/MarketList/MarketList.jsx
+++ src/Components/MarketList/MarketList.jsx
@@ -1,17 +1,33 @@
 import styles from './MarketList.module.css';
 
+import { 
+    useState,
+    useEffect
+ } from 'react';
+
 import { createArray } from './createArray';
 import MarketElement from '../MarketElement/MarketElement';
 
 export default function MarketList(){
-    const coinPerPage = 5;
+    const [currentCoins, setCurrentCoins] = useState([])
+    const coinsPerPage = 5;
     const totalCoins = 10;
-    const totalPages = Math.ceil(coinPerPage / totalCoins);
+    const totalPages = Math.ceil(coinsPerPage / totalCoins);
     const array = createArray(totalCoins);
+
+    useEffect(() => {
+        let coins = [];
+
+        for(let i = 0 ; i < coinsPerPage ; i++){
+            coins.push(array[i]);
+        }
+
+        setCurrentCoins(coins);
+    }, []);
 
     return(
         <div className={styles.marketList}>
-            {array.map((num) => <MarketElement coin={num}/>)}
+            {currentCoins.map((num) => <MarketElement coin={num}/>)}
         </div>
     );
 }
