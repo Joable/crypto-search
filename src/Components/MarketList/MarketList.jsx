bcfac40c9dc53199742b25403b0dfca946be892d
Joaquin <jaltable@fi.uba.ar>
2023-10-03 18:51:00 -0300

    'MarketList' displays the elements depending con the page number

--- src/Components/MarketList/MarketList.jsx
+++ src/Components/MarketList/MarketList.jsx
@@ -9,24 +9,32 @@ import { createArray } from './createArray';
 import MarketElement from '../MarketElement/MarketElement';
 
 export default function MarketList(){
-    const [currentCoins, setCurrentCoins] = useState([])
     const coinsPerPage = 5;
-    const totalCoins = 10;
-    const totalPages = Math.ceil(coinsPerPage / totalCoins);
+    const totalCoins = 20;
+    const totalPages = Math.ceil(totalCoins / coinsPerPage);
     const array = createArray(totalCoins);
+    const [currentCoins, setCurrentCoins] = useState([])
+    const [currentFirstElement , setCurrentFirstElement] = useState(0);
+    const [currentLastElement, setCurrentLastElement] = useState(coinsPerPage);
 
-    useEffect(() => {
+    useEffect(() =>  navigateArray(3), []);
+
+    const navigateArray = (page) => {
         let coins = [];
+        
+        setCurrentFirstElement(0 + (coinsPerPage * page));
+        setCurrentLastElement(coinsPerPage + (coinsPerPage * page));
 
-        for(let i = 0 ; i < coinsPerPage ; i++){
+        for(let i = currentFirstElement ; i < currentLastElement ; i++){
             coins.push(array[i]);
         }
 
         setCurrentCoins(coins);
-    }, []);
+    }
 
     return(
         <div className={styles.marketList}>
+            {console.log(currentCoins)}
             {currentCoins.map((num) => <MarketElement coin={num}/>)}
         </div>
     );
