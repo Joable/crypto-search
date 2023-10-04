a03e53268972dfa7b6b93a05eb9b099534fc9724
Joaquin <jaltable@fi.uba.ar>
2023-10-04 17:11:11 -0300

    added styles to 'Reason'

--- src/Components/Reason/Reason.jsx
+++ src/Components/Reason/Reason.jsx
@@ -2,16 +2,22 @@ import styles from './Reason.module.css';
 
 export default function Reason(){
     return(
-        <div style={styles.reason}>
-            <img src="../../img/smiley2.png" alt="smiley"/>
+        <div className={styles.reason}>
+            
+            <div className={styles.image}>
+                <img src="../../img/smiley2.png" alt="smiley"/>
+            </div>
 
-            <h3>Title</h3>
+            <div className={styles.reasonText}>
+                <h3>Title</h3>
 
-            <p>
-                Curabitur eget magna sit amet sem vehicula dapibus. 
-                Nullam pulvinar lorem eleifend feugiat scelerisque. 
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            </p>
+                <p>
+                    Curabitur eget magna sit amet sem vehicula dapibus. 
+                    Nullam pulvinar lorem eleifend feugiat scelerisque. 
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                </p>
+            </div>
+
         </div>
     )
 }
