87d6f310090893c2ad7313e04d1dbb6ec320bb92
Joaquin <jaltable@fi.uba.ar>
2023-10-04 16:52:39 -0300

    added 'Reason' component to 'ChooseUs'

--- src/Components/ChooseUs/ChooseUs.jsx
+++ src/Components/ChooseUs/ChooseUs.jsx
@@ -1,9 +1,19 @@
+import Reason from '../Reason/Reason';
 import styles from './ChooseUs.module.css';
 
 export default function ChooseUs(){
     return(
-        <>
-            Why Choose Us
-        </>
+        <section>
+            <h2>
+                Why Choose Us
+            </h2>
+
+            <Reason/>
+            <Reason/>
+            <Reason/>
+            <Reason/>
+            <Reason/>
+            <Reason/>
+        </section>
     );
 }
