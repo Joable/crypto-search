db619344155626a20aa3463df68132d672b7a995
Joaquin <jaltable@fi.uba.ar>
2023-10-04 16:55:08 -0300

    added content to 'Reason' component

--- src/Components/Reason/Reason.jsx
+++ src/Components/Reason/Reason.jsx
@@ -3,7 +3,13 @@ import styles from './Reason.module.css';
 export default function Reason(){
     return(
         <>
-            Reason
+            <h3>Title</h3>
+
+            <p>
+                Curabitur eget magna sit amet sem vehicula dapibus. 
+                Nullam pulvinar lorem eleifend feugiat scelerisque. 
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            </p>
         </>
     )
 }
