b42b38948459e8ac09785bc143d5c9298b40f935
Joaquin <jaltable@fi.uba.ar>
2023-10-05 09:51:36 -0300

    added content to 'JoinUs'

--- src/Components/JoinUs/JoinUs.jsx
+++ src/Components/JoinUs/JoinUs.jsx
@@ -2,8 +2,29 @@ import styles from './JoinUs.module.css';
 
 export default function JoinUs(){
     return(
-        <>
-            Join Us
-        </>
+        <section>
+            <h2>
+                JOIN US VIA
+            </h2>
+            <h2>
+                DISCORD
+            </h2>
+
+            <p>Invest and manage all your crypto in one place</p>
+
+            <button>Join via Discord</button>
+
+            <div className={styles.social}>
+                
+                <a href="#">Twitter</a>
+                
+                <a href="#">Discord</a>
+                
+                <a href="#">Facebook</a>
+                
+                <a href="#">Youtube</a>
+
+            </div>
+        </section>
     )
 }
