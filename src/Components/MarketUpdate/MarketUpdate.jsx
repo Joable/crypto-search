c58cc7964c817c9e94021cafe9b3ed9dafcec913
Joaquin <jaltable@fi.uba.ar>
2023-10-07 13:42:43 -0300

    added a global rule for the sections to center the content

--- src/Components/MarketUpdate/MarketUpdate.jsx
+++ src/Components/MarketUpdate/MarketUpdate.jsx
@@ -5,19 +5,19 @@ import MarketList from '../MarketList/MarketList';
 export default function MarketUpdate(){
     return(
         <section id='marketUpdate' className={styles.marketUpdate}>
-        <h2>Market Update</h2>
+            <h2>Market Update</h2>
 
-        <div className={styles.divisions}>
+            <div className={styles.divisions}>
 
-            <h3>Coin</h3>
+                <h3>Coin</h3>
 
-            <h3>Price</h3>
+                <h3>Price</h3>
 
-            <h3>24h change</h3>
+                <h3>24h change</h3>
 
-            <h3>Market Cap</h3>
+                <h3>Market Cap</h3>
 
-        </div>
+            </div>
 
         <MarketList/>
         </section>
