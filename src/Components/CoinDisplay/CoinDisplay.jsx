10700dfd9d44a6d133feedda4a6157d532d67d97
Joaquin <jaltable@fi.uba.ar>
2023-10-10 18:11:14 -0300

    now 'CoinDisplay' has its values given by the coinranking API

--- src/Components/CoinDisplay/CoinDisplay.jsx
+++ src/Components/CoinDisplay/CoinDisplay.jsx
@@ -1,21 +1,23 @@
+import { useEffect } from 'react';
 import styles from './CoinDisplay.module.css';
 
 export default function CoinDisplay({ coinData }){
-    const {name, change, value} = coinData
+    const coin = JSON.parse(coinData)
+    const {name, iconUrl, price, change} = coin.data.coin;
  
     return(
         <div className={styles.coin}>
+            {console.log(JSON.parse(coinData))}
 
             <div className={styles.coinImage}>
-                <img src="../../img/smiley1.png" alt="Coin" />
+                <img src={iconUrl} alt="Coin" />
             </div>
-
 
             <div className={styles.coinText}>
 
                 <h3>{name} {change}</h3>
 
-                <h3>{value}</h3>
+                <h3>{price}</h3>
             
             </div>
 
--- src/Components/LandingSection/LandingSection.jsx
+++ src/Components/LandingSection/LandingSection.jsx
@@ -1,13 +1,62 @@
 import styles from './LandingSection.module.css';
+import { 
+    useEffect,
+    useState
+} from 'react';
 
 import CoinDisplay from '../CoinDisplay/CoinDisplay';
 
+
 export default function LandingSection(){
+    const [coinsData, setCoinsData] = useState([]);
+    const coinsUuid = ["Qwsogvtv82FCd", "razxDUgYGNAdQ", "HIVsRcGKkPFtW", "a91GCGd_u96cF"];
     const coin = {
         name: "xD",
         change: "50%",
         value:"30"
-    }
+    };
+    //const url = 'https://coinranking1.p.rapidapi.com/coin/Qwsogvtv82FCd?referenceCurrencyUuid=yhjMzLPhuIDl&timePeriod=24h';
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': 'ce25898df1msheb0f33f8531834ap1d5480jsnd7ffc1659004',
+            'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
+        }
+    };
+
+    useEffect(() => {         
+        
+        let coinsUpdate = []
+
+        const fetchCoins = async () => {
+            for(let i = 0 ; i < coinsUuid.length ; i++){
+    
+                const response = await fetch(`https://coinranking1.p.rapidapi.com/coin/${coinsUuid[i]}?referenceCurrencyUuid=yhjMzLPhuIDl&timePeriod=24h`, options);
+                const result = await response.text();
+    
+                coinsUpdate.push(result);
+            }
+            console.log("1")
+        }
+
+        fetchCoins().then(() => setCoinsData(coinsUpdate));
+    }, []);
+
+    /*const fetchCoins = async () => {
+        let coinsUpdate = []
+
+        for(let i = 0 ; i < coinsUuid.length ; i++){
+
+            const response = await fetch(`https://coinranking1.p.rapidapi.com/coin/${coinsUuid[i]}?referenceCurrencyUuid=yhjMzLPhuIDl&timePeriod=24h`, options);
+            const result = await response.text();
+
+            coinsUpdate.push(result);x
+        }
+
+        console.log(coinsUpdate)
+
+        return coinsUpdate;
+    };*/
 
     return(
         <section id='landingSection' className={styles.landingSection}>
@@ -17,13 +66,7 @@ export default function LandingSection(){
 
             <div className={styles.coins}>
 
-                <CoinDisplay coinData={coin}/>
-
-                <CoinDisplay coinData={coin}/>
-
-                <CoinDisplay coinData={coin}/>
-
-                <CoinDisplay coinData={coin}/>
+                {coinsData.map((coin) => <CoinDisplay coinData={coin}/>)}
 
             </div>
 
