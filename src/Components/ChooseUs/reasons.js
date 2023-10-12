025c3bbe0406e5b9ca3538b6f3aad968af0e9442
Joaquin <jaltable@fi.uba.ar>
2023-10-12 18:25:45 -0300

    added text to 'Reason'

--- src/Components/ChooseUs/reasons.js
+++ src/Components/ChooseUs/reasons.js
@@ -1,32 +1,32 @@
 export const reasons = [
     {
         image:"../../img/ChooseUsImages/Wallet.svg",
-        title:"",
-        text:"",
+        title:"CONNECT YOUR WALLET",
+        text:"Use Trust Wallet, Metamask or to connect to the app.",
     },
     {
         image:"../../img/ChooseUsImages/Pencil.svg",
-        title:"",
-        text:"",
+        title:"SELECT YOUR QUANTITY",
+        text:"Upload your crypto and set a title, description and price.",
     },
     {
         image:"../../img/ChooseUsImages/Lightning.svg",
-        title:"",
-        text:"",
+        title:"CONFIRM TRANSACTION",
+        text:"Earn by selling your crypto on our marketplace.",
     },
     {
         image:"../../img/ChooseUsImages/Satellite.svg",
-        title:"",
-        text:"",
+        title:"RECEIVE YOUR OWN NFTS",
+        text:"Invest all your crypto at one place on one platform.",
     },
     {
         image:"../../img/ChooseUsImages/Dollar.svg",
-        title:"",
-        text:"",
+        title:"TAKE A MARKET TO SELL",
+        text:"Discover, collectthe right collections to buy or sell.",
     },
     {
         image:"../../img/ChooseUsImages/Collection.svg",
-        title:"",
-        text:"",
+        title:"DRIVE YOUR COLLECTION",
+        text:"We make it easy to discover, inves and manage.",
     },
 ]
