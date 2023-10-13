64f6dcb9981c5e3140c5afd272ef6e7b22406946
Joaquin <jaltable@fi.uba.ar>
2023-10-13 16:02:24 -0300

    now the coins are fetched at 'MarketUpdate'

--- src/Components/MarketList/MarketList.jsx
+++ src/Components/MarketList/MarketList.jsx
@@ -8,48 +8,16 @@ import {
 import { createArray } from './createArray';
 import MarketElement from '../MarketElement/MarketElement';
 
-export default function MarketList(){
+export default function MarketList({ allCoins, totalCoins }){
     const coinsPerPage = 5;
-    const totalCoins = 20;
     const totalPages = createArray(Math.ceil(totalCoins / coinsPerPage));
-    const url = `https://coinranking1.p.rapidapi.com/coins?referenceCurrencyUuid=yhjMzLPhuIDl&timePeriod=24h&tiers%5B0%5D=1&orderBy=marketCap&orderDirection=desc&limit=${totalCoins}&offset=0`;
-    const options = {
-	    method: 'GET',
-	    headers: {
-            'X-RapidAPI-Key': 'ce25898df1msheb0f33f8531834ap1d5480jsnd7ffc1659004',
-            'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
-	    }
-    };
-    const array = createArray(totalCoins);
-    const [allCoins, setAllCoins] = useState([]);
     const [currentCoins, setCurrentCoins] = useState([]);
     const [pageIndexes, setPageIndexes] = useState({firstElement: 0, lastElement: coinsPerPage});
     const [activeButton, setActiveButton] = useState(0);
-    const [isLoading, setIsLoading] = useState(true);
+    const [isLoading, setIsLoading] = useState(false);
 
     /* displays the first array elements at first mount */
-    useEffect(() => {
-        const fetchCoins = async () =>{
-            try {
-                const response = await fetch(url, options);
-                const result = await response.text();
-                const resultObject = JSON.parse(result);
-
-                setAllCoins(resultObject.data.coins);    
-                
-                handlePageChange(0);
-
-                setIsLoading(false);
-
-            } catch (error) {
-                console.error(error);
-            }
-        };
-
-        fetchCoins();
-
-        
-    }, []);
+    useEffect(() => handlePageChange(0), []);
 
     /* changes the displayed elements when the page indexes changes*/
     useEffect(() => {
--- src/Components/MarketUpdate/MarketUpdate.jsx
+++ src/Components/MarketUpdate/MarketUpdate.jsx
@@ -1,16 +1,52 @@
 import styles from './MarketUpdate.module.css';
 
+import { 
+    useState,
+    useEffect
+ } from 'react';
+
 import MarketList from '../MarketList/MarketList';
 import SortMarket from '../SortMarket/SortMarket';
 
 export default function MarketUpdate(){
+    const totalCoins = 20;
+    const url = `https://coinranking1.p.rapidapi.com/coins?referenceCurrencyUuid=yhjMzLPhuIDl&timePeriod=24h&tiers%5B0%5D=1&orderBy=marketCap&orderDirection=desc&limit=${totalCoins}&offset=0`;
+    const options = {
+	    method: 'GET',
+	    headers: {
+            'X-RapidAPI-Key': 'ce25898df1msheb0f33f8531834ap1d5480jsnd7ffc1659004',
+            'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
+	    }
+    };
+    const [allCoins, setAllCoins] = useState([]);
+
+    useEffect(() => {
+        const fetchCoins = async () =>{
+            try {
+                const response = await fetch(url, options);
+                const result = await response.text();
+                const resultObject = JSON.parse(result);
+
+                setAllCoins(resultObject.data.coins);    
+
+                //setIsLoading(false);
+
+            } catch (error) {
+                console.error(error);
+            }
+        };
+
+        fetchCoins();
+        
+    }, []);
+
     return(
         <section id='marketUpdate' className={styles.marketUpdate}>
             <h2>Market Update</h2>
 
             <SortMarket/>
 
-            <MarketList/>
+            <MarketList allCoins={allCoins} totalCoins={totalCoins}/>
         </section>
     );
 }
