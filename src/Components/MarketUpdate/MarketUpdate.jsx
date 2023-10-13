1dca1c506acae3080007816614b2e8d6f34dd918
Joaquin <jaltable@fi.uba.ar>
2023-10-13 15:54:13 -0300

    added content to 'SortMarket'

--- src/Components/MarketUpdate/MarketUpdate.jsx
+++ src/Components/MarketUpdate/MarketUpdate.jsx
@@ -1,23 +1,14 @@
 import styles from './MarketUpdate.module.css';
 
 import MarketList from '../MarketList/MarketList';
+import SortMarket from '../SortMarket/SortMarket';
 
 export default function MarketUpdate(){
     return(
         <section id='marketUpdate' className={styles.marketUpdate}>
             <h2>Market Update</h2>
 
-            <div className={styles.divisions}>
-
-                <h3>Coin</h3>
-
-                <h3>Price</h3>
-
-                <h3>24h change</h3>
-
-                <h3>Market Cap</h3>
-
-            </div>
+            <SortMarket/>
 
             <MarketList/>
         </section>
--- src/Components/SortMarket/SortMarket.jsx
+++ src/Components/SortMarket/SortMarket.jsx
@@ -3,7 +3,17 @@ import styles from './SortMarket.module.css';
 export default function SortMarket(){
     return(
         <>
-        Sort
+        <div className={styles.divisions}>
+
+            <h3>Coin</h3>
+
+            <h3>Price</h3>
+
+            <h3>24h change</h3>
+
+            <h3>Market Cap</h3>
+
+        </div>
         </>
     );
 }
