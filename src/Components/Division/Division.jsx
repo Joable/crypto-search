a8c6088138d9db856c59bd4d2036f0e8629d586b
Joaquin <jaltable@fi.uba.ar>
2023-10-15 20:01:22 -0300

    now the first order of an active 'Division' is descendant

--- src/Components/Division/Division.jsx
+++ src/Components/Division/Division.jsx
@@ -3,20 +3,20 @@ import styles from './Division.module.css';
 import { useEffect, useState } from 'react';
 
 export default function Division({title, ascendant, descendant, activeButton, setActiveButton}) {
-    const [arrowType, setArrowType] = useState(1);
+    const [arrowType, setArrowType] = useState(0);
     const [arrow, setArrow] = useState(<></>);
     
     useEffect( () => {
         if(activeButton === title){
 
             if(arrowType !== 0){
-                setArrow(<img className={styles.arrow} src='../../img/Descendant.svg'/>);
-
-                descendant();
-            }else{
                 setArrow(<img className={styles.arrow} src='../../img/Ascendant.svg'/>)
                 
                 ascendant();
+            }else{
+                setArrow(<img className={styles.arrow} src='../../img/Descendant.svg'/>);
+
+                descendant();
             };
 
         }else{
