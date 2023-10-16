03f311366a2e4b7619c349cd6f94fc1d87cc97e2
Joaquin <jaltable@fi.uba.ar>
2023-10-16 15:43:30 -0300

    added content to 'LoadingCoinDisplay'

--- src/Components/CoinDisplay/LoadingCoinDisplay.jsx
+++ src/Components/CoinDisplay/LoadingCoinDisplay.jsx
@@ -3,7 +3,13 @@ import styles from './CoinDisplay.module.css';
 export default function LoadingCoinDisplay(){
     return(
         <>
-            Loading...
+            <img src="" alt="" />
+
+            {name}
+
+            <h3>-.-- %</h3>
+
+            <h3>$ ----.--</h3>
         </>
     );
 }
